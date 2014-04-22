10f2dde48a29ffda4637976f4c6910eff5c06fda
Ben Palmer <hello@benpalmer.info>
2014-04-22 13:22:02 +0100

    add modernizr, more functions.php stuff, new grid generator LESS file

--- functions.php
+++ functions.php
@@ -4,7 +4,63 @@ if ( function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails' );
 }
 
+if ( function_exists( 'add_image_size' ) ) {
+  add_image_size( 'square', 220, 200, true );
+}
 
+if ( function_exists( 'register_nav_menus' ) ) {
+  register_nav_menus(
+    array(
+      'main_menu' => 'Main Menu'
+    )
+  );
+}
+
+/*
+  General Housekeeping
+  **************************************************************/
+
+remove_action('wp_version_check', 'wp_version_check');
+remove_action('wp_head', 'wp_generator');
+remove_action( 'wp_head', 'wlwmanifest_link');
+remove_action( 'wp_head', 'rsd_link');
+
+// Remove rubbish inline styles from header
+
+add_action( 'widgets_init', 'my_remove_recent_comments_style' );
+function my_remove_recent_comments_style() {
+  global $wp_widget_factory;
+  remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'  ) );
+}
+
+// Remove default gallery styles
+
+add_filter( 'use_default_gallery_style', '__return_false' );
+
+// Add excerpts to pages
+
+add_action( 'init', 'my_add_excerpts_to_pages' );
+function my_add_excerpts_to_pages() {
+     add_post_type_support( 'page', 'excerpt' );
+}
+
+/*
+  Add Custom Sizes to Media Upload
+  **************************************************************/
+
+function my_insert_custom_image_sizes( $sizes ) {
+  global $_wp_additional_image_sizes;
+  if ( empty($_wp_additional_image_sizes) )
+    return $sizes;
+
+  foreach ( $_wp_additional_image_sizes as $id => $data ) {
+    if ( !isset($sizes[$id]) )
+      $sizes[$id] = ucfirst( str_replace( '-', ' ', $id ) );
+  }
+
+  return $sizes;
+}
+add_filter( 'image_size_names_choose', 'my_insert_custom_image_sizes' );
 
 /*
   Enqueue Scripts and Styles
@@ -13,10 +69,10 @@ if ( function_exists( 'add_theme_support' ) ) {
 if( !is_admin()){
   // JS
   wp_deregister_script('jquery');
-  wp_enqueue_script('jquery', get_bloginfo('url')."/wp-includes/js/jquery/jquery.js", false, '1.9.1', true);
+  wp_enqueue_script('jquery', get_bloginfo('url')."/wp-includes/js/jquery/jquery.js", false, '1.11.2', true);
   wp_enqueue_script('app', get_bloginfo('template_directory')."/js/app.js", false, '1.0', true);
-  wp_enqueue_script('modernizr', get_bloginfo('template_directory')."/js/vendor/modernizr-2.6.2.min.js", true, '2.6.2', false);
-  wp_enqueue_script( array('modernizr','jquery','backbone','underscore','app') );
+  wp_enqueue_script('modernizr', get_bloginfo('template_directory')."/js/vendor/modernizr.min.js", true, '2.7.2', false);
+  wp_enqueue_script( array('modernizr','jquery','underscore','app') );
   // CSS
   wp_enqueue_script( 'styles', get_bloginfo('template_directory')."/css/style.css", false, '1.0', false );
 }
--- index.php
+++ index.php
@@ -1,17 +1,3 @@
 <?php get_header(); ?>
 
-  <div class="container test">
-    <div class="row">
-      <div class="span3"></div>
-      <div class="span3"></div>
-      <div class="span3"></div>
-      <div class="span3"></div>
-    </div>
-    <div class="row">
-      <div class="span4"></div>
-      <div class="span4"></div>
-      <div class="span4"></div>
-    </div>
-  </div>
-
 <?php get_footer(); ?>
